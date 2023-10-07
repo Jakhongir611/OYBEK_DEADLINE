f31f4cdd0df39331448c8bb56cd2a6113b7695ed
jakhingir611 <khurramovjahongir611@gmail.com>
2023-10-07 20:31:04 +0500

    👌

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
 let doc = document;
 let body = doc.body;
-let div = doc.createElement('div');
-let catalog = doc.createElement('catalog');
-let vibor = doc.createElement('vibor');
+let catalog = doc.querySelector('.catalog');
+let vibor = doc.querySelector('.vibor');
+let btn_tel = doc.querySelector('.tel');
+let btn_pc = doc.querySelector('.pc');
 let market_phone = doc.querySelector('.market_phone');
 let market_pc = doc.querySelector('.market_pc');
 
 catalog.addEventListener('click', () => {
     vibor.classList.remove('none');
+})
+
+btn_tel.addEventListener('click', () => {
+    market_pc.classList.add('none');
+    market_phone.classList.remove('none');
+    vibor.classList.add('none')
+})
+btn_pc.addEventListener('click', () => {
+    market_phone.classList.add('none');
+    market_pc.classList.remove('none');
+    vibor.classList.add('none')
 })
